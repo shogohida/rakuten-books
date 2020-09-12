ed8b6a8ba73f455911f0cf71549f2cb9a9b92c12
Shogo Hida <shogo.hida@gmail.com>
2020-09-12 22:28:38 +0900

    created models

---
+++ db/migrate/20200912132802_create_books.rb
@@ -0,0 +1,14 @@
+class CreateBooks < ActiveRecord::Migration[6.0]
+  def change
+    create_table :books do |t|
+      t.string :title
+      t.string :image
+      t.text :description
+      t.string :price
+      t.references :publisher, null: false, foreign_key: true
+      t.references :author, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
